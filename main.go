c6cbc6565687c9936c659c850cb3249a55c50ca5
Luuk Kemp <luuk.kemp@simac.com>
2020-02-13 10:41:46 +0100

    wip

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ func main() {
 	var authToken string
 	var oldTag string
 	var newTag string
+	var registry string
 	if len(os.Args) < 3 {
 		fmt.Println("This application requires at least 2 parameters")
 		fmt.Printf("Example: %s register.example.com/nginx:dev register.example.com/nginx:1.0\n", os.Args[0])
@@ -34,12 +35,12 @@ func main() {
 	}
 	oldTag = os.Args[1]
 	newTag = os.Args[2]
-	if os.Getenv("REGISTRY") == "" {
-		fmt.Println("This application requires at least a registry environment variable")
-		fmt.Println("Example: export REGISTRY=https://register.example.com")
-		os.Exit(1)
+	if os.Getenv("REGISTRY") != "" {
+		registry = os.Getenv("REGISTRY")
+	} else {
+		fmt.Println("Registry env var not found, assuming docker hub")
+		registry = "https://registry-1.docker.io"
 	}
-	registry := os.Getenv("REGISTRY")
 	if os.Getenv("REGISTRY_USER") == "" && os.Getenv("REGISTRY_PASSWORD") == "" {
 		fmt.Println("REGISTRY_USER and REGISTRY_PASSWORD environment variable not found, assuming unauthenticated")
 		authenticated = false
@@ -49,13 +50,14 @@ func main() {
 		registryUser = os.Getenv("REGISTRY_USER")
 		registryPassword = os.Getenv("REGISTRY_PASSWORD")
 		authToken = getAuthToken(registry, registryUser, registryPassword)
+		fmt.Println(authToken)
 	}
-	image, body := fetchManifest(registry, oldTag)
+	image, body := fetchManifest(registry, authToken, oldTag)
 	setTag(registry, authToken, image, body, newTag)
 
 }
 
-func fetchManifest(registry string, tag string) (string, []byte) {
+func fetchManifest(registry string, authToken string, tag string) (string, []byte) {
 	s := strings.Split(tag, ":")
 	var image string
 	var version string
@@ -70,7 +72,12 @@ func fetchManifest(registry string, tag string) (string, []byte) {
 	fmt.Println("Image:", image, version)
 	fmt.Println("Authentication:", registryUser, registryPassword)
 	request, _ := http.NewRequest("GET", registry+"/v2/"+image+"/manifests/"+version, nil)
+	fmt.Println(registry+"/v2/"+image+"/manifests/"+version, nil)
 	request.Header.Set("Accept", "application/vnd.docker.distribution.manifest.v2+json")
+	if authToken != "" {
+		request.Header.Set("Authorization", "JWT "+authToken)
+	}
+	fmt.Println(authToken)
 	client := &http.Client{}
 	r, err := client.Do(request)
 	if err != nil {
@@ -78,6 +85,7 @@ func fetchManifest(registry string, tag string) (string, []byte) {
 	} else {
 		if r.StatusCode != 200 && r.StatusCode != 201 {
 			fmt.Println("Error fetching manifest")
+			fmt.Println(r.StatusCode)
 		} else {
 			body, _ := ioutil.ReadAll(r.Body)
 			return image, body
@@ -112,15 +120,28 @@ func setTag(registry string, authToken string, image string, manifest []byte, ne
 	}
 }
 
-func getAuthToken(registry, username, password string) string {
+func getAuthToken(registry, username, password, image string) (string, string) {
 	var token AuthToken
+	var readToken string
+	client := &http.Client{}
 	requestBody, _ := json.Marshal(map[string]string{
 		"username": username,
 		"password": password,
 	})
+	if registry == "https://registry-1.docker.io" {
+		registry = "https://auth.docker.io/token?service=registry.docker.io&scope=repository:" + username + "/" + image + ":pull"
+		request, _ := http.NewRequest("GET", registry, nil)
+		resp, err := client.Do(request)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer resp.Body.Close()
+		body, _ := ioutil.ReadAll(resp.Body)
+		err = json.Unmarshal([]byte(string(body)), &token)
+		readToken = token.Token
+	}
 	request, _ := http.NewRequest("POST", registry+"/v2/users/login/", bytes.NewBuffer(requestBody))
 	request.Header.Set("Content-type", "application/json")
-	client := &http.Client{}
 	resp, err := client.Do(request)
 	if err != nil {
 		fmt.Println(err)
@@ -131,5 +152,5 @@ func getAuthToken(registry, username, password string) string {
 	if err != nil {
 		fmt.Println(err)
 	}
-	return token.Token
+	return readToken, token.Token
 }
