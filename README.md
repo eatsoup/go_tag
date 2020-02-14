408aaca7cb9cb8ccc8e93f798052ef7760bac222
Eatsoup <eatsoup@gmail.com>
2020-02-14 13:21:17 +0100

    Working docker hub

---
+++ README.md
@@ -0,0 +1,16 @@
+# GO TAG
+## Simple tool to tag a remote docker image without pulling it first
+
+## How to use
+### Tool
+```bash
+git clone https://github.com/eatsoup/go_tag.git
+cd go_tag && go build .
+export REGISTRY_USER='username'
+export REGISTRY_PASSWORD='password'
+./go_tag repository/image:oldtag repository_image:newtag
+```
+### Docker image
+```bash
+docker run --rm -t -e REGISTRY_USER='username' -e REGISTRY_PASSWORD='password' eatsoup/go_tag go_tag repository/image:old_tag repository/image:new_tag
+```
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -19,7 +20,7 @@ var authenticated bool
 var registryUser string
 var registryPassword string
 
-type AuthToken struct {
+type authToken struct {
 	Token string `json:"token"`
 }
 
@@ -35,12 +36,7 @@ func main() {
 	}
 	oldTag = os.Args[1]
 	newTag = os.Args[2]
-	if os.Getenv("REGISTRY") != "" {
-		registry = os.Getenv("REGISTRY")
-	} else {
-		fmt.Println("Registry env var not found, assuming docker hub")
-		registry = "https://registry-1.docker.io"
-	}
+	registry = os.Getenv("REGISTRY")
 	if os.Getenv("REGISTRY_USER") == "" && os.Getenv("REGISTRY_PASSWORD") == "" {
 		fmt.Println("REGISTRY_USER and REGISTRY_PASSWORD environment variable not found, assuming unauthenticated")
 		authenticated = false
@@ -49,35 +45,33 @@ func main() {
 		authenticated = true
 		registryUser = os.Getenv("REGISTRY_USER")
 		registryPassword = os.Getenv("REGISTRY_PASSWORD")
-		authToken = getAuthToken(registry, registryUser, registryPassword)
-		fmt.Println(authToken)
+		authToken = getAuthToken(registry, registryUser, registryPassword, oldTag)
 	}
 	image, body := fetchManifest(registry, authToken, oldTag)
-	setTag(registry, authToken, image, body, newTag)
+	setTag(registry, authToken, image, newTag, body)
 
 }
 
-func fetchManifest(registry string, authToken string, tag string) (string, []byte) {
+func fetchManifest(registry, authToken, tag string) (string, []byte) {
+	if registry == "" {
+		registry = "https://registry-1.docker.io"
+	}
 	s := strings.Split(tag, ":")
 	var image string
 	var version string
 	if len(s) > 1 {
-		image = strings.Join(s[0:1], ":")
+		image = s[0]
 		version = s[len(s)-1]
 	} else {
 		image = tag
 		version = "latest"
 	}
-	fmt.Println("Registry:", registry)
-	fmt.Println("Image:", image, version)
-	fmt.Println("Authentication:", registryUser, registryPassword)
-	request, _ := http.NewRequest("GET", registry+"/v2/"+image+"/manifests/"+version, nil)
-	fmt.Println(registry+"/v2/"+image+"/manifests/"+version, nil)
+	url := registry + "/v2/" + image + "/manifests/" + version
+	request, _ := http.NewRequest("GET", url, nil)
 	request.Header.Set("Accept", "application/vnd.docker.distribution.manifest.v2+json")
 	if authToken != "" {
-		request.Header.Set("Authorization", "JWT "+authToken)
+		request.Header.Set("Authorization", "Bearer "+authToken)
 	}
-	fmt.Println(authToken)
 	client := &http.Client{}
 	r, err := client.Do(request)
 	if err != nil {
@@ -94,13 +88,25 @@ func fetchManifest(registry string, authToken string, tag string) (string, []byt
 	return image, nil
 }
 
-func setTag(registry string, authToken string, image string, manifest []byte, newVersion string) {
-	fmt.Printf("Tagging %s with version: %s\n", image, newVersion)
+func setTag(registry, authToken, image, newVersion string, manifest []byte) {
+	var version string
 	var response string
-	request, _ := http.NewRequest("PUT", registry+"/v2/"+image+"/manifests/"+newVersion, bytes.NewBuffer(manifest))
+	if registry == "" {
+		registry = "https://registry-1.docker.io"
+	}
+	s := strings.Split(newVersion, ":")
+	if len(s) > 1 {
+		image = s[0]
+		version = s[len(s)-1]
+	} else {
+		version = "latest"
+	}
+	fmt.Printf("Tagging %s with version: %s\n", image, version)
+	url := registry + "/v2/" + image + "/manifests/" + version
+	request, _ := http.NewRequest("PUT", url, bytes.NewBuffer(manifest))
 	request.Header.Set("Content-Type", "application/vnd.docker.distribution.manifest.v2+json")
 	if authToken != "" {
-		request.Header.Set("Authorization", "JWT "+authToken)
+		request.Header.Set("Authorization", "Bearer "+authToken)
 	}
 	client := &http.Client{}
 	r, err := client.Do(request)
@@ -120,17 +126,17 @@ func setTag(registry string, authToken string, image string, manifest []byte, ne
 	}
 }
 
-func getAuthToken(registry, username, password, image string) (string, string) {
-	var token AuthToken
+func getAuthToken(registry, username, password, tag string) string {
+	var token authToken
 	var readToken string
+	image := strings.Split(tag, ":")[0]
 	client := &http.Client{}
-	requestBody, _ := json.Marshal(map[string]string{
-		"username": username,
-		"password": password,
-	})
-	if registry == "https://registry-1.docker.io" {
-		registry = "https://auth.docker.io/token?service=registry.docker.io&scope=repository:" + username + "/" + image + ":pull"
+	credentials := base64.StdEncoding.EncodeToString([]byte(username + ":" + password))
+	if registry == "" {
+		registry = "https://auth.docker.io/token?scope=repository:" + image + ":pull,push&service=registry.docker.io"
 		request, _ := http.NewRequest("GET", registry, nil)
+		request.Header.Set("Content-type", "application/json")
+		request.Header.Set("Authorization", "Basic "+credentials)
 		resp, err := client.Do(request)
 		if err != nil {
 			fmt.Println(err)
@@ -140,17 +146,5 @@ func getAuthToken(registry, username, password, image string) (string, string) {
 		err = json.Unmarshal([]byte(string(body)), &token)
 		readToken = token.Token
 	}
-	request, _ := http.NewRequest("POST", registry+"/v2/users/login/", bytes.NewBuffer(requestBody))
-	request.Header.Set("Content-type", "application/json")
-	resp, err := client.Do(request)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-	err = json.Unmarshal([]byte(string(body)), &token)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return readToken, token.Token
+	return readToken
 }
